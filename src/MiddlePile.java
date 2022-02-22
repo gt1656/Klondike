2d50763506c78bc2485d79608b07c481a2b25b1d
Ruby Harkness <rubyharkness@Rubys-MacBook-Air-2.local>
2022-02-22 10:14:49 -0800

    pile subclasses/class

---
+++ src/MiddlePile.java
@@ -0,0 +1,29 @@
+import java.awt.Graphics;
+import java.awt.event.ActionEvent;
+
+public class MiddlePile extends Pile{
+
+    public MiddlePile(int x, int y) {
+        super(x, y);
+        //TODO Auto-generated constructor stub
+    }
+
+    @Override
+    public void update(ActionEvent a) {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public void draw(Graphics g) {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public boolean canAddCard(Card c) {
+        // TODO Auto-generated method stub
+        return false;
+    }
+    
+}
--- src/Pile.java
+++ src/Pile.java
@@ -1,4 +1,7 @@
 
+import java.util.ArrayList;
+
+
 /** A Pile is a collection of cards.  This needs to be
  * Drawable because it will be shown on the GUI. Put code
  * here that ALL Piles share.  The ways in which Piles are 
@@ -8,5 +11,40 @@
  */
 public abstract class Pile implements Drawable, Updateable {
     
+    private int x, y; 
+    private ArrayList<Card> list; 
     public abstract boolean canAddCard(Card c);
+    // private int numCards;
+    // private int maxCards; 
+
+
+    public Pile(int x, int y){
+        // this.numCards = numCards; 
+        // this.maxCards = maxCards;
+        this.x = x;
+        this.y = y;
+    }
+
+    private int getX(){
+        return x;
+    }
+
+    private int getY(){
+        return y; 
+    }
+
+    public int getSize(){
+        return list.size();
+    }
+
+    // private int getnumCards(){
+    //     return numCards;
+    // }
+
+    // private int maxCards(){
+    //     return maxCards;
+    // }
+
+
 }
+
