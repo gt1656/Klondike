2cf612770ca6dee6392bb2c93179d166fecd94e0
Ruby Harkness <rubyharkness@Rubys-MacBook-Air-2.local>
2022-02-22 10:24:58 -0800

    comment

--- src/Pile.java
+++ src/Pile.java
@@ -1,7 +1,4 @@
 
-import java.util.ArrayList;
-
-
 /** A Pile is a collection of cards.  This needs to be
  * Drawable because it will be shown on the GUI. Put code
  * here that ALL Piles share.  The ways in which Piles are 
@@ -10,41 +7,7 @@ import java.util.ArrayList;
  * You WILL write subclasses of Pile
  */
 public abstract class Pile implements Drawable, Updateable {
+    //comment
     
-    private int x, y; 
-    private ArrayList<Card> list; 
     public abstract boolean canAddCard(Card c);
-    // private int numCards;
-    // private int maxCards; 
-
-
-    public Pile(int x, int y){
-        // this.numCards = numCards; 
-        // this.maxCards = maxCards;
-        this.x = x;
-        this.y = y;
-    }
-
-    private int getX(){
-        return x;
-    }
-
-    private int getY(){
-        return y; 
-    }
-
-    public int getSize(){
-        return list.size();
-    }
-
-    // private int getnumCards(){
-    //     return numCards;
-    // }
-
-    // private int maxCards(){
-    //     return maxCards;
-    // }
-
-
 }
-
